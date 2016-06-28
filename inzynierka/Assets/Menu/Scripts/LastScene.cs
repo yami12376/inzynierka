f1a97c6d34c091a8b5cf6f5088c03f7f13418695
kamil <Kamil_wit1@wp.pl>
2016-06-28 22:16:41 +0200

    początkowa scena Menu i scena Info

---
+++ inzynierka/Assets/Menu/Scripts/LastScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class LastScene
+{
+
+	private static string lastScene;
+
+	public static string myLastScene {
+		get { return lastScene; }
+		set { lastScene = value; }
+	}
+
+}
---
+++ inzynierka/Assets/Menu/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour {
+
+	public void LoadScene (string level)
+	{
+		LastScene.myLastScene = Application.loadedLevelName;
+		Application.LoadLevel (level);
+	}
+
+	public void Exit ()
+	{
+
+		Application.Quit ();
+	}
+}
