d23f03693216bc17e386392e477326c0cb8ebba9
kamil <Kamil_wit1@wp.pl>
2016-08-09 20:53:16 +0200

    Poczatek animacji chodzenia głównego bohatera.

---
+++ inzynierka/Assets/Department/Scripts/HeroMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class HeroMovement : MonoBehaviour
+{
+	
+	public float speed = 0.1f;
+
+
+	void FixedUpdate ()
+	{
+		if (Input.GetKey ("up"))
+			this.GetComponent<Transform> ().transform.position += new Vector3 (0, speed, 0);
+
+		if (Input.GetKey ("down"))
+			this.GetComponent<Transform> ().transform.position -= new Vector3 (0, speed, 0);
+
+		if (Input.GetKey ("left"))
+			this.GetComponent<Transform> ().transform.position -= new Vector3 (speed, 0, 0);
+
+		if (Input.GetKey ("right"))
+			this.GetComponent<Transform> ().transform.position += new Vector3 (speed, 0, 0);
+	}
+}
