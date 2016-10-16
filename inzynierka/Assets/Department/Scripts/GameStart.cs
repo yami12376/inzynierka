c67348584d34feebbb7cfcf43c16dd74eaeea696
kamil <Kamil_wit1@wp.pl>
2016-10-16 22:34:01 +0200

    Numery sal sa generowane losowo na podstawie skryptu i pokazywane na ścianie przy wejściu do poszczególnych klas

---
+++ inzynierka/Assets/Department/Scripts/GameStart.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Linq;
+
+public class GameStart : MonoBehaviour
+{
+	private GameObject[] classNumbers;
+	private System.Random rnd = new System.Random ();
+
+	void Start ()
+	{
+
+		var randomNumbers = Enumerable.Range (1, 9).OrderBy (x => rnd.Next ()).Take (9).ToList ();
+	
+		classNumbers = GameObject.FindGameObjectsWithTag ("Class Number");
+
+		int i = 0;
+
+		foreach (GameObject classNumber in classNumbers) {
+			classNumber.GetComponentInChildren<Text> ().text = "sala200" + randomNumbers.ElementAt (i);
+			Debug.Log (randomNumbers.ElementAt (i));
+			i++;
+		}
+	}
+}
