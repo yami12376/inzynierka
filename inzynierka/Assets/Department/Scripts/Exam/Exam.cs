c0e6635e868386e741fb0a703097d05a11374737
kamil <Kamil_wit1@wp.pl>
2016-11-19 14:17:24 +0100

    Wprowadzenie opcji strzału. Wyświetlenie komunikatu kończącego egzamin, kiedy kończy się ilość pytań.

--- inzynierka/Assets/Department/Scripts/Exam/Exam.cs
+++ inzynierka/Assets/Department/Scripts/Exam/Exam.cs
@@ -13,6 +13,9 @@ public class Exam : MonoBehaviour {
 	private Button cheatButton;
 	private Button shootButton;
 	private Button moveButton;
+	private Button goBackToMenuButton;
+
+	private int correctAnswers;
 
 	private int currentQuestionNumber;
 
@@ -23,6 +26,9 @@ public class Exam : MonoBehaviour {
 		cheatButton = buttonComponents [0];
 		shootButton = buttonComponents [1];
 		moveButton = buttonComponents [2];
+		goBackToMenuButton = buttonComponents [3];
+
+		goBackToMenuButton.gameObject.SetActive (false);
 
 		currentQuestionNumber = 1;
 	}
@@ -72,9 +78,52 @@ public class Exam : MonoBehaviour {
 
 		Debug.Log ("points " + points);
 
-		points += 65;
 
 		if (points > 60) {
+			correctAnswers++;
+			return true;
+		} else {
+			return false;
+		}
+
+
+	}
+
+	private bool DoesCheatSucced(){
+
+		Random.Range (0f, 50f);
+
+		// cheat max 10 pkt * 5
+
+
+		float points = Stats.Cheating * 5 + Random.Range (0f, 50f);
+
+		Debug.Log ("points " + points);
+
+		if (points > 60) {
+			correctAnswers++;
+			return true;
+		} else {
+			return false;
+		}
+
+
+	}
+
+	private bool DoesShootSucced(){
+
+
+		//  4 odpowiedzi = 25%
+
+
+		float points = Random.Range (0f, 100f);
+
+		Debug.Log ("points " + points);
+
+
+
+		if (points > 50) {
+			correctAnswers++;
 			return true;
 		} else {
 			return false;
@@ -85,16 +134,56 @@ public class Exam : MonoBehaviour {
 
 	public void ChangeQuestion(){
 
-		textComponents [0].text = "bla bla";
+		currentQuestionNumber++;
 
-
-		changedTextOfThisQuestion = false;
 
 		shootButton.interactable = false;
 		cheatButton.interactable = false;
 		moveButton.interactable = false;
 
-		currentQuestionNumber++;
+		Debug.Log ("current question NUmber = " + currentQuestionNumber);
+
+		// tu dać text z nastepnego pytania
+		if (currentQuestionNumber > questionsNumber) {
+			
+			if (correctAnswers == 1) {
+				if (questionsNumber / 2 <= correctAnswers) {
+					textComponents [0].text = "koniec egzaminu. Postać odpowiedziała poprawnie: " + correctAnswers + " raz. Na " +
+						questionsNumber + " pytań. Zdałeś egzamin. Koniec gry, udaj się do menu, aby zagrać ponownie.";
+				} else {
+					
+					textComponents [0].text = "koniec egzaminu. Postać odpowiedziała poprawnie: " + correctAnswers + " raz. Na " +
+						questionsNumber + " pytań. Nie Zdałeś egzaminu. Koniec gry, udaj się do menu, aby zagrać ponownie.";
+				}
+			} else {
+				if (questionsNumber / 2 <= correctAnswers) {
+					textComponents [0].text = "koniec egzaminu. Postać odpowiedziała poprawnie: " + correctAnswers + " razy. Na " +
+						questionsNumber + " pytań. Zdałeś egzamin. Koniec gry, udaj się do menu, aby zagrać ponownie.";
+				} else {
+
+					textComponents [0].text = "koniec egzaminu. Postać odpowiedziała poprawnie: " + correctAnswers + " razy. Na " +
+						questionsNumber + " pytań. Nie Zdałeś egzaminu. Koniec gry, udaj się do menu, aby zagrać ponownie.";
+				}
+			}
+
+			shootButton.interactable = false;
+			cheatButton.interactable = false;
+			moveButton.interactable = false;
+			textComponents [4].text = "";
+			goBackToMenuButton.gameObject.SetActive (true);
+
+	
+		}
+		else{
+			changedTextOfThisQuestion = false;
+
+			textComponents [0].text = "bla bla";
+
+		}
+
+
+	
+
 
 		Debug.Log ("teraz pytanie numer = " + currentQuestionNumber);
 
@@ -104,6 +193,64 @@ public class Exam : MonoBehaviour {
 		}
 
 	}
+
+	public void Cheat(){
+
+		// changedTextOfThisQuestion = false;
+
+		shootButton.interactable = false;
+		cheatButton.interactable = false;
+		moveButton.interactable = false;
+
+		bool cheatSucced = DoesCheatSucced ();
+
+		Debug.Log ("cheat udało się " + cheatSucced);
+
+		if (!cheatSucced) {
+
+			textComponents [0].text = "Zostałeś przyłapany na ściąganiu. Nie zdałeś egzaminu.";
+
+			goBackToMenuButton.gameObject.SetActive (true);
+
+		} else {
+			textComponents [4].text = "Udało Ci się sciągnąć !";
+			moveButton.interactable = true;
+		}
+	
+
+		Debug.Log ("teraz pytanie numer = " + currentQuestionNumber);
+
+		if (currentQuestionNumber > questionsNumber) {
+
+			// zmiana sceny do wyniku
+		}
+
+	}
+
+
+	public void Shoot(){
+
+		// changedTextOfThisQuestion = false;
+
+		shootButton.interactable = false;
+		cheatButton.interactable = false;
+
+		bool shootSucced = DoesShootSucced ();
+
+		Debug.Log ("Strzał się udał? " + shootSucced);
+
+		moveButton.interactable = true;
+
+
+	}
+
+
+	public void GoToMenu(){
+
+		Application.LoadLevel ("Menu");
+
+	}
+
 
 
 }
