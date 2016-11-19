bf873f315ffffa9d5f9d1d9f04d10e2eeb3b0c13
kamil <Kamil_wit1@wp.pl>
2016-11-19 13:11:57 +0100

    Dodanie skryptu odpowiedzialnego za egzamin

--- inzynierka/Assets/Department/Scripts/Exam/Exam.cs
+++ inzynierka/Assets/Department/Scripts/Exam/Exam.cs
@@ -4,11 +4,35 @@ using UnityEngine.UI;
 
 public class Exam : MonoBehaviour {
 
-	public float questionsNumber;
+	public int questionsNumber;
+	private bool changedTextOfThisQuestion;
 
+	private Text[] textComponents;
+	private Button[] buttonComponents;
+
+	private Button cheatButton;
+	private Button shootButton;
+	private Button moveButton;
+
+	private int currentQuestionNumber;
+
+	void Start(){
+		textComponents = this.GetComponentsInChildren<Text> ();
+	    buttonComponents = this.GetComponentsInChildren<Button> ();
+
+		cheatButton = buttonComponents [0];
+		shootButton = buttonComponents [1];
+		moveButton = buttonComponents [2];
+
+		currentQuestionNumber = 1;
+	}
+		
 	// Update is called once per frame
 	void Update () {
-		Text textComponent = this.GetComponentInChildren<Text> ();
+		
+		Text textComponent = textComponents [0];
+		Text knowAnswerText = textComponents [4];
+
 		string text = textComponent.text;
 		char questionNumber = text [0];
 		Debug.Log ("number = " + questionNumber);
@@ -16,6 +40,70 @@ public class Exam : MonoBehaviour {
 		Debug.Log("cheatStat = " + Stats.Cheating);
 		Debug.Log("Wisdomtat = " + Stats.Knowledge);
 
+
+
+		if (!changedTextOfThisQuestion) {
+			changedTextOfThisQuestion = true;
+			if (doesHeroKnowAnswer ()) {
+				Debug.Log ("zna");
+				knowAnswerText.text = "Postać zna odpowiedź";
+				moveButton.interactable = true;
+
+			} else {
+				Debug.Log ("nie zna");
+				knowAnswerText.text = "Postać nie zna odpowiedzi";
+				shootButton.interactable = true;
+				cheatButton.interactable = true;
+			}
+		}
+
 		
 	}
+
+
+	private bool doesHeroKnowAnswer(){
+
+		Random.Range (0f, 50f);
+
+		// wiedza max 10 pkt * 5
+
+
+		float points = Stats.Knowledge * 5 + Random.Range (0f, 50f);
+
+		Debug.Log ("points " + points);
+
+		points += 45;
+
+		if (points > 60) {
+			return true;
+		} else {
+			return false;
+		}
+
+
+	}
+
+	public void changeQuestion(){
+
+		textComponents [0].text = "bla bla";
+
+
+		changedTextOfThisQuestion = false;
+
+		shootButton.interactable = false;
+		cheatButton.interactable = false;
+		moveButton.interactable = false;
+
+		currentQuestionNumber++;
+
+		Debug.Log ("teraz pytanie numer = " + currentQuestionNumber);
+
+		if (currentQuestionNumber > questionsNumber) {
+
+			// zmiana sceny do wyniku
+		}
+
+	}
+
+
 }
