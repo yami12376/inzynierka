1bac0af1f7b59bfda876f22d2928b05de99465d1
kamil <Kamil_wit1@wp.pl>
2016-11-20 11:59:07 +0100

    Możliwość przejścia dalej - nawet kiedy nie przydzieliło się wszystkich statystyk.

--- inzynierka/Assets/Menu/Scripts/Stats/StatsChanging.cs
+++ inzynierka/Assets/Menu/Scripts/Stats/StatsChanging.cs
@@ -1,65 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StatsChanging : MonoBehaviour {
+public class StatsChanging : MonoBehaviour
+{
 
 	public string levelToLoad;
 
-		void Start()
-		{
-			Stats.Cheating = 0;
-			Stats.Knowledge = 0;
-			Stats.Remaining = 15;
-			Stats.StartCheating = 0;
-			Stats.StartKnowledge = 0;
+	void Start ()
+	{
+		Stats.Cheating = 0;
+		Stats.Knowledge = 0;
+		Stats.Remaining = 15;
+		Stats.StartCheating = 0;
+		Stats.StartKnowledge = 0;
+	}
+
+	public void KnowledgeUp ()
+	{
+		if (Stats.Remaining > 0 && Stats.Knowledge < Stats.Max) {
+			Stats.Remaining -= 1;
+			Stats.Knowledge += 1;
 		}
 
-		public void KnowledgeUp()
-		{
-			if (Stats.Remaining > 0 && Stats.Knowledge < Stats.Max)
-			{
-				Stats.Remaining -= 1;
-				Stats.Knowledge += 1;
-			}
+	}
 
-		}
-
-		public void KnowledgeDown()
-		{
-			if (Stats.Remaining < Stats.Points && Stats.Knowledge > Stats.StartKnowledge)
-			{
-				Stats.Remaining += 1;
-				Stats.Knowledge -= 1;
-			}
-		}
-		
-		public void CheatingUp()
-		{
-			if (Stats.Remaining > 0 && Stats.Cheating < Stats.Max)
-			{
-				Stats.Remaining -= 1;
-				Stats.Cheating += 1;
-			}
-		}
-
-		public void CheatingDown()
-		{
-			if (Stats.Remaining < Stats.Points && Stats.Cheating > Stats.StartCheating)
-			{
-				Stats.Remaining += 1;
-				Stats.Cheating -= 1;
-			}
-		}
-
-		public void Submit()
-		{
-			if (Stats.Remaining == 0)
-			{
-				Stats.StartCheating = Stats.Cheating;
-				Stats.StartKnowledge = Stats.Knowledge;
-
-			Application.LoadLevel (levelToLoad);
-			}
+	public void KnowledgeDown ()
+	{
+		if (Stats.Remaining < Stats.Points && Stats.Knowledge > Stats.StartKnowledge) {
+			Stats.Remaining += 1;
+			Stats.Knowledge -= 1;
 		}
 	}
+
+	public void CheatingUp ()
+	{
+		if (Stats.Remaining > 0 && Stats.Cheating < Stats.Max) {
+			Stats.Remaining -= 1;
+			Stats.Cheating += 1;
+		}
+	}
+
+	public void CheatingDown ()
+	{
+		if (Stats.Remaining < Stats.Points && Stats.Cheating > Stats.StartCheating) {
+			Stats.Remaining += 1;
+			Stats.Cheating -= 1;
+		}
+	}
+
+	public void Submit ()
+	{
+
+		Stats.StartCheating = Stats.Cheating;
+		Stats.StartKnowledge = Stats.Knowledge;
+
+		Application.LoadLevel (levelToLoad);
+	}
+}
 
